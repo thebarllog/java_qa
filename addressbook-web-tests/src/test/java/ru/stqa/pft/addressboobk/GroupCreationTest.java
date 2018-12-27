e44035dcae1ccc6b1b43d854137003ab67cc9003
Mikhail <barllog@hotmail.com>
2018-12-27 18:11:39 +0700

    Logging has been moved to fixture

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressboobk/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressboobk/GroupCreationTest.java
@@ -12,10 +12,6 @@ public class GroupCreationTest {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/group.php");
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
@@ -23,6 +19,10 @@ public class GroupCreationTest {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
